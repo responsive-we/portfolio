70af1f07fe339caf1f06c3829644e3c3c0ae41d2
responsive-we <98676194+responsive-we@users.noreply.github.com>
2023-11-06 18:43:48 +0530

    Adding assets in src folder

---
+++ src/assets/certificates/index.js
@@ -0,0 +1,5 @@
+import node_course from './node_course.jpg';
+import react_course from './react_course.jpg';
+import github_course from './github_course.jpg';
+import html_css_js_course from './html_css_js_course.jpg';
+export {node_course,react_course,github_course,html_css_js_course}
