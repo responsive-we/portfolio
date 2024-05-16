a8ac22851a9269181bb2857ff5e013b0d2fb27a4
responsive-we <98676194+responsive-we@users.noreply.github.com>
2024-05-16 17:17:11 +0530

    Improved responsivness

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { BallCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
-
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motions";
+import { styles } from "../styles";
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center'>{
+    <div className='flex flex-col flex-wrap justify-center'>
+        <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText}`}>My skills</p>
+        <h2 className={`${styles.sectionHeadText}`}>Technologies.</h2>
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center">
+      {
       technologies.map((tech, index) => (
         <div className='w-28 h-28 ' key={tech.name}>
-          <BallCanvas
-            icon={tech.icon}
-          />
+        <img src={tech.icon} alt="" className='w-full h-auto filter brightness-125 ' />
         </div>
       ))}
+      </div>
     </div>
   )
 }
